bdd84dbfbf1c8fd365c316922be5f8a6083ce659
YB.Creates( ) <38274423+yash1399@users.noreply.github.com>
2020-07-02 20:21:46 +0530

    Add files via upload

---
+++ build/precache-manifest.d6adf7ab0f0d2e91f53ca04c11f883a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76b76c21d764f6c36d930a6432c0be3e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4eeb9e35ac1351f698ee",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "524672126c25d300e91e",
+    "url": "/static/js/2.37cb0cc3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.37cb0cc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4eeb9e35ac1351f698ee",
+    "url": "/static/js/main.2765a80c.chunk.js"
+  },
+  {
+    "revision": "743ed7aa369908501510",
+    "url": "/static/js/runtime-main.a30a588e.js"
+  }
+]);
